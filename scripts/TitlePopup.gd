374ed4290a7b1853f79294bac267d60294d5e2eb
wutdahack <70644502+wutdahack@users.noreply.github.com>
2022-09-20 09:21:06 +0100

    cleanup TitlePopup.gd comments

--- scripts/TitlePopup.gd
+++ scripts/TitlePopup.gd
@@ -1,21 +1,8 @@
 extends Control
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	get_tree().set_pause(true) # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
+	get_tree().set_pause(true)
 
 func _on_PlayButton_pressed():
 	get_tree().set_pause(false)
-	queue_free() # Replace with function body.
+	queue_free()
